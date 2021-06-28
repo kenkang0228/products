66c5142bac0e1bd0baa3dece4ced2c77b5ff615e
Ken Kang <kenkang0228@gmail.com>
2021-06-28 21:01:32 +0800

    add with open

--- products.py
+++ products.py
@@ -8,3 +8,10 @@ while True:
 	price = input('輸入價錢: ')
 	products.append([name, price]) #建立小清單加入大清單
 print(products)
+
+for p in products:
+	print(p[0], '的價錢是', p[1])
+
+with open('products.csv', 'w') as f:
+	for p in products:
+		f.write(p[0] + ',' + p[1] + '\n')
